8911ee5a71b45ca5e0b2167df7daa6870ac48dda
amith-george <amithgeorge130@gmail.com>
2025-09-24 22:03:12 +0530

    Status controller added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const roomRoutes = require('./routes/room.routes');
 const messageRoutes = require('./routes/message.routes');
 const youtubeRoutes = require('./routes/youtube.routes');
 const twitchRoutes = require('./routes/twitch.routes');
+const statusRoutes = require('./routes/status.routes');
 
 app.use(cors());
 app.use(express.json());
@@ -19,5 +20,6 @@ app.use('/rooms', roomRoutes);
 app.use('/messages', messageRoutes);
 app.use('/youtube', youtubeRoutes);
 app.use('/twitch', twitchRoutes);
+app.use('/status', statusRoutes);
 
 module.exports = app;
---
+++ controllers/status.controller.js
@@ -0,0 +1,9 @@
+// controllers/status.controller.js
+
+exports.getStatus = async (req, res) => {
+    try {
+        res.status(200).json({ message: 'Backend is active', status: 'ok' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
